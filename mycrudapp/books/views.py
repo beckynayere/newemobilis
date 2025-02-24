325e563b2977b7616ea48bedc66466eb564d2801
beckynayere <rebecca.nayere@gmail.com>
2025-02-24 16:14:41 +0300

    create template

--- mycrudapp/books/views.py
+++ mycrudapp/books/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
-from .forms import BookForm
+from .Forms import BookForm
 
 # Read (List All Books)
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'books/book_list.html', {'books': books})
+    return render(request, 'books/templates/book_list.html', {'books': books})
 
 # Create New Book
 def book_create(request):
@@ -16,7 +16,7 @@ def book_create(request):
             return redirect('book_list')
     else:
         form = BookForm()
-    return render(request, 'books/book_form.html', {'form': form})
+    return render(request, 'books/templates/book_form.html', {'form': form})
 
 # Update Book
 def book_update(request, pk):
@@ -28,7 +28,7 @@ def book_update(request, pk):
             return redirect('book_list')
     else:
         form = BookForm(instance=book)
-    return render(request, 'books/book_form.html', {'form': form})
+    return render(request, 'books/templates/book_form.html', {'form': form})
 
 # Delete Book
 def book_delete(request, pk):
@@ -36,4 +36,4 @@ def book_delete(request, pk):
     if request.method == 'POST':
         book.delete()
         return redirect('book_list')
-    return render(request, 'books/book_confirm_delete.html', {'book': book})
+    return render(request, 'books/templates/book_confirm_delete.html', {'book': book})
